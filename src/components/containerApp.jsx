26d9c06896f51ecd2d7bc7d63b1fd6cc34332c08
jmanzanares12 <jimgken2004@gmail.com>
2025-01-31 17:02:27 -0600

    Add: folder components with file main

---
+++ src/components/containerApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContainerApp = () => {
+    return (
+        <div>        
+            <h1>Password Generator</h1>
+            <p>This is a password generator</p>
+        </div>
+    )
+}
+
+export default ContainerApp;
