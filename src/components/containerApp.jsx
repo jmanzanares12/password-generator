8185e30a2f5b376aa9a730c73d587de6b2b42ae0
jmanzanares12 <jimgken2004@gmail.com>
2025-02-03 17:22:55 -0600

    Add: logic to generate password

--- src/components/containerApp.jsx
+++ src/components/containerApp.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 
 const ContainerApp = () => {
+    const [password, setPassword] = useState('');
+
+    const generatePassword = () => {
+        const length = 12;
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
+        let newPassword = '';
+        for (let i = 0; i < length; i++) {
+            newPassword += characters.charAt(Math.floor(Math.random() * characters.length));
+        }
+        setPassword(newPassword);
+    }
+
     return (
         <div>        
             <h1>Password Generator</h1>
             <div className="passwordContainer">
                 <div className="passwordBox">
-                    <input type="text" placeholder="Password" />
+                    <input type="text" placeholder="Password" value={password} readOnly />
+                    <button onClick={generatePassword}>Generate</button>
                     <button>Generate</button>
                 </div>
             </div>
